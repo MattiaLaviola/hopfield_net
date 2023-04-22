692ede0231b68c77f998cab29350734d5c13e60e
MattiaLaviola <10699411@polimi.it>
2023-04-22 10:20:55 +0200

    First commit, template set up, and ready to go

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ fn main() -> eframe::Result<()> {
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
-        "eframe template",
+        "hopfield nets",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(hopfield_nets:TemplateApp::new(cc))),
     )
 }
 
